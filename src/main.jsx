f10311065f06951b302295b85dc783284d64a047
Eugene Oliveros <yujin@jinyuz.dev>
2024-10-13 13:15:13 +0800

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/beybrew",
+    element: <App />
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
