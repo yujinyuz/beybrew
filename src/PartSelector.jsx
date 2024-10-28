f0bc3d9a7f3550873ae4153f6f6c05e1627f302e
Eugene Oliveros <yujin@jinyuz.dev>
2024-10-28 22:49:57 +0800

    feat: use react-select for better ux

--- src/PartSelector.jsx
+++ src/PartSelector.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select'
 
 import { BEYBLADE_DB, LIMITED_FORMAT } from './constants';
 
@@ -12,21 +13,21 @@ function PartSelector({ label, options, value, onChange, partsUsed, currentForma
     })
   }
 
+  const formattedOptions = options.map((option) => {
+    return {
+      value: option,
+      label: `${option} ${BEYBLADE_DB[option].alias ? `(${BEYBLADE_DB[option].alias})` : ''} ${currentFormat === LIMITED_FORMAT ? (BEYBLADE_DB[option].points || '???') : ''}`
+    }
+  })
+
   return (
     <div className="mb-4">
       <label className="block text-sm font-medium text-gray-700 mb-2">{label}</label>
-      <select
-        className="block w-full border-gray-300 rounded-md shadow-sm"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      >
-        <option value="">Select {label}</option>
-        {options.map((option) => (
-          <option key={option} value={option} disabled={partsUsed.includes(option)}>
-            {option} {BEYBLADE_DB[option].alias ? `(${BEYBLADE_DB[option].alias})` : null} {currentFormat === LIMITED_FORMAT ? (BEYBLADE_DB[option].points || '???') : null}
-          </option>
-        ))}
-      </select>
+
+      <Select
+        className='block w-full border-gray-300 rounded-md shadow-sm'
+        onChange={(e) => onChange(e.value)}
+        options={formattedOptions} />
     </div>
   );
 }
